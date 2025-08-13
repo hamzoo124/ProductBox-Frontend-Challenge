e115e95a280a63535cb88c83f2ef8a1998fcf1ce
Sahibzada Hamza sardar <71062903+hamzoo124@users.noreply.github.com>
2025-08-13 12:46:12 +0500

    integrated React frontend with Node backend

--- index.js
+++ index.js
@@ -1,43 +1,55 @@
-var debug = require('debug')('frontend-code-challenge');
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var logger = require('./lib/logger');
+var debug = require("debug")("frontend-code-challenge");
+var express = require("express");
+const cors = require("cors");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var bodyParser = require("body-parser");
+var logger = require("./lib/logger");
 
-var items = require('./routes/items');
+var items = require("./routes/items");
 
 var app = express();
 var log = logger(app);
 
 app.use(bodyParser.json());
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'static')));
 
-app.use('/items', items);
+// ✅ Serve React build folder
+const frontendPath = path.join(__dirname, "static/frontend/build");
+app.use(express.static(frontendPath));
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+// API routes
+app.use("/items", items);
+app.use("/img", express.static(path.join(__dirname, "static/img")));
+
+// ✅ React catch-all route
+app.get("*", (req, res) => {
+  res.sendFile(path.join(frontendPath, "index.html"));
 });
 
-// development error handler
-// will print stacktrace
-app.use(function(err, req, res, next) {
-    log.error(err);
-    res.status(err.status || 500);
-    res.json({
-        message: err.message,
-        error: err
-    });
+// 404 handler
+app.use(function (req, res, next) {
+  var err = new Error("Not Found");
+  err.status = 404;
+  next(err);
 });
 
-app.set('port', process.env.PORT || 3000);
+// Error handler
+app.use(function (err, req, res, next) {
+  log.error(err);
+  res.status(err.status || 500).json({
+    message: err.message,
+    error: err,
+  });
+});
 
-var server = app.listen(app.get('port'), function() {
-    log.info('Express server listening on http://localhost:%d', server.address().port);
+app.set("port", process.env.PORT || 3000);
+
+var server = app.listen(app.get("port"), function () {
+  log.info(
+    "Express server listening on http://localhost:%d",
+    server.address().port
+  );
 });
